3333e29cc459a2fa067e0b720d9caf8b59142ed8
Kevin <kevin_smith1292@yahoo.com>
2023-10-03 13:29:45 -0500

    finally

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(saturday='roller-skate'){
+    return `This Saturday, I want to ${saturday}!`
+}
+
+function mondayWork (monday='go to the office'){
+    return `This Monday, I will ${monday}.`
+}
+
+function wrapAdjective(happy2= '*'){
+    return function(happy = 'special'){
+        return `You are ${happy2}${happy}${happy2}!`
+    }
+}
+console.log(wrapAdjective("banana")("totally"))
